f30ac86eed9860e57640da33c251dd055c99b8de
billdelvin <billdelvin6@gmail.com>
2020-02-11 14:45:34 +0700

    change route name and add process

--- backend/router/todo.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const todoControllers = require('../controllers/todo')
-
-router.post('/create', todoControllers.create)
-module.exports = router
---
+++ backend/router/todos.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const controllers = require("../controllers/todo");
+
+//delete data
+router.delete("/:id", controllers.delete);
+
+// show data
+router.get("/", controllers.get);
+
+//toggle
+router.patch("/:id", controllers.patch);
+
+// create data
+router.post("/", controllers.post);
+
+module.exports = router;
