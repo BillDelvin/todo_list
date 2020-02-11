fdde55b4c8f4b8fee1ac335dd6adb9c800b3bf3b
billdelvin <billdelvin6@gmail.com>
2020-02-11 14:43:47 +0700

    adding controll

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -1,13 +1,34 @@
 const Todo = require("../models/todo");
+
 module.exports = {
-  create: ((req, res) => {
-    const newTodo = new Todo({
-      title: req.body.title
+  delete: (req, res) => {
+    Todo.findByIdAndDelete(req.params.id).then(() =>
+      res.json({ remove: true })
+    );
+  },
+  get: (req, res) => {
+    Todo.find({})
+      .then(todo => res.json(todo))
+      .catch(err => console.log(err));
+  },
+  patch: (req, res) => {
+    const { id } = req.params;
+
+    Todo.findByIdAndUpdate(id, req.body).then(response => {
+      console.log(response);
+      res.json({ remove: true });
     });
+  },
+  post: (req, res) => {
+    // const newTodo = new Todo({
+    //   title: req.body.title
+    // });
+
+    const newTodo = new Todo(req.body);
+
     newTodo
       .save()
       .then(todo => res.json(todo))
       .catch(err => console.log(err));
-  }),
-//   showData:
+  }
 };
